fce16734712328061e38af98fc54b224c3a98e62
Tiền Võ <tienvo@meu-solutions.com>
2022-05-29 21:41:18 +0700

    update

--- careerproject/settings.py
+++ careerproject/settings.py
@@ -128,7 +128,7 @@ TEMPLATES = [
 #
 # ]
 
-# WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
