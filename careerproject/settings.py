2d8562f32064a77737272b4d7dcc693585deffbe
Tiền Võ <tienvo@meu-solutions.com>
2022-05-29 21:20:39 +0700

    update

--- careerproject/settings.py
+++ careerproject/settings.py
@@ -13,17 +13,11 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import mimetypes
 
 mimetypes.add_type("text/css", ".css", True)
-#
-# django_heroku.settings(locals(), staticfiles=False)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
